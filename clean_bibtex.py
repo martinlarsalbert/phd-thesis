44e1aea0c4835dcbdf85caaeef5494c32d836726
Martin Alexandersson <martin.alexandersson@sspa.se>
2022-11-10 11:00:47 +0100

    Lower case in reference titles.

--- clean_bibtex.py
+++ clean_bibtex.py
@@ -15,5 +15,17 @@ for remove in removes:
     logger.info(f'remove {remove}')
     s = re.sub(remove + r' *= .*\{[^}]+},','',s, re.DOTALL)
 
+"""
+Lower case titles
+Ex:
+title = {Dynamics of a {Rigid} {Ship} - with applications}
+change to:
+title = {Dynamics of a {rigid} {ship} - with applications}
+"""
+for result in re.finditer(r"title *= *\{(.*)\}", s):
+    old = result.group(1)
+    new = old[0].upper() + old[1:].lower()
+    s = s.replace(old,new)
+
 with open('library.bib', mode='w', encoding='utf8') as file:
     file.write(s)
