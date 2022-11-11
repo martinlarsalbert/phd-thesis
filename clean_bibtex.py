006721a3a003c60d0447797d5da7675a81aa86c2
Martin Alexandersson <martin.alexandersson@sspa.se>
2022-11-11 08:51:03 +0100

    removed notes and publisher except for books.

--- clean_bibtex.py
+++ clean_bibtex.py
@@ -1,21 +1,39 @@
 import re
 import logging
+import bibtexparser
 logger = logging.getLogger('simple_example')
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
 logger.addHandler(ch)
 
-with open('library_raw.bib', mode='r', encoding='utf8') as file:
-    s = file.read()
+
+with open('library_raw.bib',mode='r', encoding='utf8') as bibtex_file:
+    bib_database = bibtexparser.load(bibtex_file)
 
 #removes = ['publisher','note']
-removes = ['note']
+#removes = ['note']
+#for remove in removes:
+#    logger.info(f'remove {remove}')
+#    s = re.sub(remove + r' *= .*\{[^}]+},','',s, re.DOTALL)
 
 
-for remove in removes:
-    logger.info(f'remove {remove}')
-    s = re.sub(remove + r' *= .*\{[^}]+},','',s, re.DOTALL)
+def remove(id, field):
+    if field in bib_database.entries_dict[id]:
+        bib_database.entries_dict[id].pop(field)
+
+logger.info(f'Remove note for all')
+for id in bib_database.entries_dict.keys():
+    remove(id, 'note')
+
+logger.info(f'Remove publisher, except for books') 
+for id,item in bib_database.entries_dict.items():
+    if item['ENTRYTYPE']!='book':
+        remove(id, 'publisher')
+
+with open(r'library.bib',mode='w', encoding='utf8') as bibtex_file:
+    bib_database = bibtexparser.dump(bib_database, bibtex_file)
+
 
 """
 Lower case titles
@@ -24,6 +42,8 @@ title = {Dynamics of a {Rigid} {Ship} - with applications}
 change to:
 title = {Dynamics of a {rigid} {ship} - with applications}
 """
+with open('library.bib', mode='r', encoding='utf8') as file:
+    s = file.read()
 for result in re.finditer(r"title *= *\{(.*)\}", s):
     old = result.group(1)
     new = old[0].upper() + old[1:].lower()
