ca3e4d16b7f289d70353fcc964afca1d53c39d87
martinlarsalbert <marale@kth.se>
2025-01-01 12:14:21 +0100

    quotes.

--- obsidian_to_latex.py
+++ obsidian_to_latex.py
@@ -30,9 +30,21 @@ def remove_highlight(s:str)->str:
     
     s_latex = str(s)
     
-    for result in re.finditer(r"\=\=([^\]]+)==",s):
+    for result in re.finditer(r"\=\=([^\=]+)==",s):
                 
         s_latex = s_latex.replace(result.group(0),result.group(1))
+
+    return s_latex
+
+def convert_quotes(s:str)->str:
+    
+    s_latex = str(s)
+    
+    for result in re.finditer(r'\"([^\"]+)"',s):
+        
+        quote_latex = fr"\say{{{result.group(1)}}}"
+        
+        s_latex = s_latex.replace(result.group(0),quote_latex)
 
     return s_latex
     
@@ -44,6 +56,7 @@ if __name__ == '__main__':
     s_latex = convert_references(s=s)
     s_latex = remove_links(s=s_latex)
     s_latex = remove_highlight(s=s_latex)
+    s_latex = convert_quotes(s=s_latex)
     
     save_path = os.path.join("kappa","")     
     with open("./kappa/literature_review_obsidian.tex", encoding="utf8", mode='w') as file:
